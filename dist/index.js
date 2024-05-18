a8c3c0a285084af772e8bf88946d2405a21b821e
Michael Wells <info@sygnal.com>
2024-05-18 23:28:07 +0000

    Basic implementation complete

--- dist/index.js
+++ dist/index.js
@@ -17,15 +17,31 @@
       window.addEventListener("message", function(event) {
         console.log("event received");
         console.log(event.data);
+        if (event.data.type == "Enquiry")
+          event.data.numberOfPeople = "1";
+        event.data.childPrice = "100";
+        console.log(event.data);
         const { date, numberOfPeople } = event.data;
         if (typeof date === "string" && typeof numberOfPeople === "string") {
+          const bookingTypeElement = document.getElementById("booking-type");
           const dateElement = document.getElementById("tour-date");
           const peopleElement = document.getElementById("adults");
-          if (dateElement && peopleElement) {
-            dateElement.value = date;
-            peopleElement.value = numberOfPeople;
-          } else {
+          if (!(bookingTypeElement && dateElement && peopleElement)) {
             console.log("One or more form elements were not found!");
+            return;
+          }
+          dateElement.value = date;
+          peopleElement.value = numberOfPeople;
+          peopleElement.min = numberOfPeople;
+          bookingTypeElement.disabled = false;
+          switch (event.data.type) {
+            case "Book Tour Now":
+              bookingTypeElement.value = "Booking";
+              break;
+            case "Enquiry":
+              bookingTypeElement.value = "Enquiry";
+              bookingTypeElement.disabled = true;
+              break;
           }
         } else {
           console.log("Received data is not in the expected format!");
--- src/page/tour.ts
+++ src/page/tour.ts
@@ -29,19 +29,46 @@ export class TourPage {
       //   return;
       // }
     
+      // Fix event data
+      if(event.data.type == 'Enquiry')
+        event.data.numberOfPeople = '1';
+      event.data.childPrice = '100';
+
+      console.log(event.data); 
+
+      /**
+       * Apply tour data
+       */
+
       const { date, numberOfPeople } = event.data;
       
       // Check if the values exist and are of the expected types
       if (typeof date === 'string' && typeof numberOfPeople === 'string') {
+        const bookingTypeElement = document.getElementById('booking-type') as HTMLSelectElement | null;
         const dateElement = document.getElementById('tour-date') as HTMLInputElement | null;
         const peopleElement = document.getElementById('adults') as HTMLInputElement | null;
-    
-        if (dateElement && peopleElement) {
-          dateElement.value = date;
-          peopleElement.value = numberOfPeople;
-        } else {
+
+        // Verify setup
+        if (!(bookingTypeElement && dateElement && peopleElement)) {
           console.log('One or more form elements were not found!');
+          return;
         }
+    
+        dateElement.value = date;
+        peopleElement.value = numberOfPeople;
+        peopleElement.min = numberOfPeople;
+
+        bookingTypeElement.disabled = false;
+        switch(event.data.type) {
+          case 'Book Tour Now':
+            bookingTypeElement.value = 'Booking'; 
+            break;
+          case 'Enquiry':
+            bookingTypeElement.value = 'Enquiry'; 
+            bookingTypeElement.disabled = true;
+            break;
+        }
+
       } else {
         console.log('Received data is not in the expected format!');
       }
